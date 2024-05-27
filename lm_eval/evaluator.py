e54880469acf9c7abf799e79642f77f7ded75626
Fuwen Tan <fuwen.tan@gmail.com>
2024-05-27 12:28:16 +0100

    no need to print the first prompt

--- lm_eval/evaluator.py
+++ lm_eval/evaluator.py
@@ -266,12 +266,12 @@ def evaluate(
             if write_out:
                 prompt_details.append({"doc_id": doc_id})
 
-            # print the prompt for the first few documents
-            if doc_id < 1:
-                print(
-                    f"Task: {task_name}; document {doc_id}; context prompt (starting on next line):\n{ctx}\n(end of prompt on previous line)"
-                )
-                print("Requests:", reqs)
+            # # print the prompt for the first few documents
+            # if doc_id < 1:
+            #     print(
+            #         f"Task: {task_name}; document {doc_id}; context prompt (starting on next line):\n{ctx}\n(end of prompt on previous line)"
+            #     )
+            #     print("Requests:", reqs)
 
             if not isinstance(reqs, (list, tuple)):
                 reqs = [reqs]
